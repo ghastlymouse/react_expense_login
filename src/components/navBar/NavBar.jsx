296188207888bf5b260af30f1f09102cd0778c10
Ghastly Mouse <gusdnr0839@gmail.com>
2024-06-10 22:53:36 +0900

    style : 내브바 UI 및 스타일 지정

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import * as S from './NavBar.styled'
+import { Link, useNavigate } from 'react-router-dom'
 
 const NavBar = () => {
+    const navigate = useNavigate();
+
     return (
-        <div>NavBar</div>
+        <S.Container>
+            <S.LeftDiv>
+                <Link to={"/"}>Home</Link>{" "}
+                내 프로필
+            </S.LeftDiv>
+            <S.RightDiv>
+                내 닉네임{" "}
+                <S.Button>로그아웃</S.Button>
+            </S.RightDiv>
+
+        </S.Container>
     )
 }
 
---
+++ src/components/navBar/NavBar.styled.jsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    width: 100%;
+    background-color: white;
+    height: 40px;
+    padding: 20px;
+    border: 5px solid black;
+    border-radius: 8px;
+    margin: 0 10px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+`;
+
+export const LeftDiv = styled.div`
+    width: 50%;
+    height: 40px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+`;
+
+export const RightDiv = styled.div`
+    width: 50%;
+    height: 40px;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-around;
+`;
+
+export const Button = styled.button`
+    border: none;
+    border-radius: 4px;
+    background-color: ${props => props.$color || "blue"};
+    padding: 10px 20px;
+    width: 20%;
+    color: white;
+    font-family: inherit;
+    font-size: 16px;
+    cursor: pointer;
+    &:hover{
+        filter:brightness(0.8);
+    }
+`;
