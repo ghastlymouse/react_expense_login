1fcdc8a2b895a2da6e2177cecd7ae9197457ea37
Ghastly Mouse <gusdnr0839@gmail.com>
2024-06-10 21:55:53 +0900

    update : 로그인 폼 UI 변경

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import * as S from './LoginForm.styled'
+import { useNavigate } from 'react-router-dom'
 
 const LoginForm = () => {
+    const navigate = useNavigate();
+
     return (
-        <div>LoginForm</div>
+        <S.Section>
+            <S.Form>
+                <S.TitleText>Login</S.TitleText>
+                <S.InputDiv>
+                    <label>아이디</label>
+                    <S.Input type="text" />
+                </S.InputDiv>
+                <S.InputDiv>
+                    <label>비밀번호</label>
+                    <S.Input type="password" />
+                </S.InputDiv>
+                <S.ButtonDiv>
+                    <S.Button>로그인</S.Button>
+                    <S.Button $color="green" onClick={() => navigate('/signup')}>회원가입</S.Button>
+                </S.ButtonDiv>
+            </S.Form>
+        </S.Section>
     )
 }
 
