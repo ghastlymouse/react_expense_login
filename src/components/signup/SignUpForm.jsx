e54925ff49efe6f04b32fea1fe74b489d00a7fd1
Ghastly Mouse <gusdnr0839@gmail.com>
2024-06-10 22:07:02 +0900

    style : 회원가입 폼 스타일 지정

--- src/components/signup/SignUpForm.jsx
+++ src/components/signup/SignUpForm.jsx
@@ -1,8 +1,57 @@
-import React from 'react'
+import React, { useState } from 'react'
+import * as S from './signUpForm.styled'
 
 const SignUpForm = () => {
+    const [email, setEmail] = useState("");
+    const [pw, setPw] = useState("");
+    const [pwConfirm, setPwConfirm] = useState("");
+    const [nickName, setNickName] = useState("");
+
     return (
-        <div>SignUpForm</div>
+        <S.Section>
+            <S.Form>
+                <S.TitleText>Sign Up</S.TitleText>
+                <S.InputDiv>
+                    <label htmlFor="email">이메일</label>
+                    <S.Input
+                        id="email"
+                        type="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                </S.InputDiv>
+                <S.InputDiv>
+                    <label htmlFor="pw">비밀번호</label>
+                    <S.Input
+                        id="pw"
+                        type="password"
+                        value={pw}
+                        onChange={(e) => setPw(e.target.value)}
+                    />
+                </S.InputDiv>
+                <S.InputDiv>
+                    <label htmlFor="pwConfirm">비밀번호 확인</label>
+                    <S.Input
+                        id="pwConfirm"
+                        type="password"
+                        value={pwConfirm}
+                        onChange={(e) => setPwConfirm(e.target.value)}
+                    />
+                </S.InputDiv>
+                <S.InputDiv>
+                    <label htmlFor="nickName">닉네임</label>
+                    <S.Input
+                        id="nickName"
+                        type="text"
+                        value={nickName}
+                        onChange={(e) => setNickName(e.target.value)}
+                    />
+                </S.InputDiv>
+                <S.ButtonDiv>
+                    <S.Button $color="green">회원가입</S.Button>
+                </S.ButtonDiv>
+            </S.Form>
+        </S.Section>
     )
 }
 
---
+++ src/components/signup/signUpForm.styled.jsx
@@ -0,0 +1,63 @@
+import styled from "styled-components";
+
+export const Section = styled.section`
+    width: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+export const TitleText = styled.span`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 25px;
+`;
+
+export const Form = styled.form`
+    background-color: white;
+    width: 50%;
+    height: 500px;
+    border: 5px solid black;
+    border-radius: 10px;
+    display: flex;
+    flex-direction: column;
+    padding: 20px;
+    gap: 30px;
+`;
+
+export const InputDiv = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const Input = styled.input`
+    border: 3px solid black;
+    border-radius: 10px;
+    background-color: #e2dbdb;
+    height: 35px;
+    font-family: inherit;
+    font-size: inherit;
+`;
+
+export const ButtonDiv = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+`;
+
+export const Button = styled.button`
+    border: none;
+    border-radius: 4px;
+    background-color: ${props => props.$color || "blue"};
+    padding: 10px 20px;
+    width: 20%;
+    height: 40px;
+    color: white;
+    font-family: inherit;
+    font-size: 16px;
+    cursor: pointer;
+    &:hover{
+        filter:brightness(0.8);
+    }
+`;
