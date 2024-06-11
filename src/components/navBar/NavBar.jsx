532e542dfbb953991882b02358e14a9df7160216
Ghastly Mouse <gusdnr0839@gmail.com>
2024-06-11 23:34:32 +0900

    update : 로그아웃 기능 추가, navigator로 페이지 연결 각 버튼별 조건부렌더링 추가

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,22 +1,60 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import * as S from './NavBar.styled'
 import { Link, useNavigate } from 'react-router-dom'
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, setUserInfo } from '../../redux/slices/auth.slice';
+import axios from 'axios';
 
 const NavBar = () => {
     const navigate = useNavigate();
+    const dispatch = useDispatch();
+    const { isLogin } = useSelector(state => state.auth);
+    const { userInfo } = useSelector(state => state.auth);
+    const nickname = userInfo?.nickname;
+    console.log(userInfo);
+    const [username, setUsername] = useState("");
+
+    const handleLogout = () => {
+        dispatch(logout());
+    };
+
+    useEffect(() => {
+        if (!isLogin) {
+            setUsername("");
+        } else {
+            const fetchUserInfo = async () => {
+                try {
+                    const token = localStorage.getItem("accessToken");
+                    const response = await axios.get("https://moneyfulpublicpolicy.co.kr/user",
+                        {
+                            headers: {
+                                Authorization: `Bearer ${token}`,
+                            },
+                        }
+                    );
+                    console.log(response.data.nickname);
+                    setUsername(response.data.nickname);
+                    dispatch(setUserInfo(response.data));
+                } catch (error) {
+                    console.log("Failed to fetch user info:", error);
+                }
+            };
+            fetchUserInfo();
+        }
+    }, [isLogin, navigate])
 
     return (
         <S.Container>
             <S.LeftDiv>
                 <S.NavButton onClick={() => navigate("/")}>Home</S.NavButton>{" "}
-                <S.NavButton onClick={() => navigate("/mypage")}>내 프로필</S.NavButton>
+                <S.NavButton $display={isLogin ? "block" : "none"} onClick={() => navigate("/mypage")}>내 프로필</S.NavButton>
 
             </S.LeftDiv>
             <S.RightDiv>
-                내 닉네임
-                <S.Button onClick={() => navigate("/login")}>로그인</S.Button>
-                <S.Button onClick={() => navigate("/signup")}>회원가입</S.Button>
-                <S.Button>로그아웃</S.Button>
+                <S.Span $display={isLogin ? "block" : "none"}>{nickname}</S.Span>
+                <S.Button $display={isLogin ? "none" : "block"} onClick={() => navigate("/login")}>로그인</S.Button>
+                <S.Button $display={isLogin ? "none" : "block"} onClick={() => navigate("/signup")}>회원가입</S.Button>
+                <S.Button $display={isLogin ? "block" : "none"} onClick={() => handleLogout()}>로그아웃</S.Button>
             </S.RightDiv>
 
         </S.Container>
