a5516729796debca00430485bc1bd186ba766504
Ghastly Mouse <gusdnr0839@gmail.com>
2024-06-11 23:35:08 +0900

    update : JWT 닉네임 수정 기능 추가

--- src/components/mypage/MyProfile.jsx
+++ src/components/mypage/MyProfile.jsx
@@ -1,13 +1,58 @@
-import React from 'react'
+import React, { useState } from 'react'
 import * as S from './MyProfile.styled'
+import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
+import { setUserInfo } from '../../redux/slices/auth.slice';
 
 const MyProfile = () => {
+    const dispatch = useDispatch();
+
+    const [newName, setNewName] = useState("");
+    const { userInfo } = useSelector(state => state.auth);
+    const nickname = userInfo?.nickname;
+
+    const handleChangNickname = async (event) => {
+        event.preventDefault();
+        try {
+            const token = localStorage.getItem("accessToken");
+            const response = await axios.patch("https://moneyfulpublicpolicy.co.kr/profile",
+                {
+                    nickname: newName,
+                },
+                {
+                    headers: {
+                        Authorization: `Bearer ${token}`,
+                        "Content-Type": "multipart/form-data",
+                    },
+                }
+            );
+            console.log(response);
+            if (response.data.success) {
+                dispatch(setUserInfo({ ...userInfo, nickname: newName }));
+                alert("닉네임이 변경되었습니다.");
+                setNewName("");
+            } else {
+                alert("닉네임 변경에 실패했습니다.");
+            }
+        } catch (error) {
+            console.error("Failed to update nickname:", error);
+            alert("닉네임 변경에 실패했습니다.");
+        }
+    };
+
     return (
         <S.Section>
-            <S.Form>
-                <label>현재 닉네임 : </label>
-                <S.Input type="text" />
-                <S.Button>변경하기</S.Button>
+            <S.Form onSubmit={handleChangNickname}>
+                <label>현재 닉네임 : {nickname}</label>
+                <S.Input
+                    type="text"
+                    value={newName}
+                    onChange={(e) => setNewName(e.target.value)}
+                    required
+                    minLength={1}
+                    maxLength={10}
+                />
+                <S.Button type="submit">변경하기</S.Button>
             </S.Form>
         </S.Section>
     )
