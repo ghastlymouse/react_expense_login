e3194baca119020b681c5de538839ead3f11b9e5
Ghastly Mouse <gusdnr0839@gmail.com>
2024-06-12 01:12:30 +0900

    update : 닉네임 리덕스로 관리

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -12,35 +12,29 @@ const NavBar = () => {
     const { userInfo } = useSelector(state => state.auth);
     const nickname = userInfo?.nickname;
     console.log(userInfo);
-    const [username, setUsername] = useState("");
 
     const handleLogout = () => {
         dispatch(logout());
     };
 
     useEffect(() => {
-        if (!isLogin) {
-            setUsername("");
-        } else {
-            const fetchUserInfo = async () => {
-                try {
-                    const token = localStorage.getItem("accessToken");
-                    const response = await axios.get("https://moneyfulpublicpolicy.co.kr/user",
-                        {
-                            headers: {
-                                Authorization: `Bearer ${token}`,
-                            },
-                        }
-                    );
-                    console.log(response.data.nickname);
-                    setUsername(response.data.nickname);
-                    dispatch(setUserInfo(response.data));
-                } catch (error) {
-                    console.log("Failed to fetch user info:", error);
-                }
-            };
-            fetchUserInfo();
-        }
+        const fetchUserInfo = async () => {
+            try {
+                const token = localStorage.getItem("accessToken");
+                const response = await axios.get("https://moneyfulpublicpolicy.co.kr/user",
+                    {
+                        headers: {
+                            Authorization: `Bearer ${token}`,
+                        },
+                    }
+                );
+                console.log(response.data.nickname);
+                dispatch(setUserInfo(response.data));
+            } catch (error) {
+                console.log("Failed to fetch user info:", error);
+            }
+        };
+        fetchUserInfo();
     }, [isLogin, navigate])
 
     return (
@@ -51,7 +45,7 @@ const NavBar = () => {
 
             </S.LeftDiv>
             <S.RightDiv>
-                <S.Span $display={isLogin ? "block" : "none"}>{nickname}</S.Span>
+                <S.Span $display={isLogin ? "block" : "none"}>{nickname}님 환영합니다!</S.Span>
                 <S.Button $display={isLogin ? "none" : "block"} onClick={() => navigate("/login")}>로그인</S.Button>
                 <S.Button $display={isLogin ? "none" : "block"} onClick={() => navigate("/signup")}>회원가입</S.Button>
                 <S.Button $display={isLogin ? "block" : "none"} onClick={() => handleLogout()}>로그아웃</S.Button>
