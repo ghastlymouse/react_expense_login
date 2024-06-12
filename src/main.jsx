c7f142ff72a24117f4ef2b0bc574fde89fa59dfb
Ghastly Mouse <gusdnr0839@gmail.com>
2024-06-12 20:14:35 +0900

    script : tanastack-query 설치

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,19 @@ import App from './App.jsx'
 import './index.css'
 import { Provider } from "react-redux";
 import store from "./redux/config/configStore.js";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
+const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false}>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </ReactQueryDevtools>
+    </QueryClientProvider>
   </React.StrictMode>
 )
