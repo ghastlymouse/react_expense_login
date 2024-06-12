381deeb57b5fca53ed81a9368b7335fb36ade780
Ghastly Mouse <gusdnr0839@gmail.com>
2024-06-12 11:57:19 +0900

    style : 프로필 페이지 스타일 수정

--- src/components/mypage/MyProfile.styled.jsx
+++ src/components/mypage/MyProfile.styled.jsx
@@ -2,18 +2,19 @@ import styled from "styled-components";
 
 export const Section = styled.section`
     width: 100%;
-    height: 200px;
-    margin-top: 100px;
+    margin-top: 50px;
     display: flex;
+    flex-direction: column;
     align-items: center;
     justify-content: center;
+    gap: 10px;
 `;
 
 export const Form = styled.form`
     background-color: white;
     width: 40%;
     border: 5px solid black;
-    border-radius: 10px;
+    border-radius: 8px;
     display: flex;
     flex-direction: column;
     align-items: center;
@@ -27,6 +28,7 @@ export const InputDiv = styled.div`
 `;
 
 export const Input = styled.input`
+    width: 100%;
     border: 3px solid black;
     border-radius: 10px;
     background-color: #e2dbdb;
@@ -40,7 +42,7 @@ export const Button = styled.button`
     border-radius: 4px;
     background-color: ${props => props.$color || "blue"};
     padding: 10px 20px;
-    width: 20%;
+    width: 30%;
     height: 40px;
     color: white;
     font-family: inherit;
@@ -49,4 +51,10 @@ export const Button = styled.button`
     &:hover{
         filter:brightness(0.8);
     }
+`;
+
+export const Image = styled.img`
+    width: 200px;
+    height: 200px;
+    border-radius: 100px;
 `;
