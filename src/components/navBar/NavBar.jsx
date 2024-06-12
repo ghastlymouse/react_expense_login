94171c6e2a1f84b678e03ddf26d58cdfbd6ccd68
Ghastly Mouse <gusdnr0839@gmail.com>
2024-06-12 11:59:15 +0900

    update : 내브바에 프로필 이미지 추가

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -11,11 +11,15 @@ const NavBar = () => {
     const { isLogin } = useSelector(state => state.auth);
     const { userInfo } = useSelector(state => state.auth);
     const nickname = userInfo?.nickname;
+    const avatar = userInfo?.avatar;
     console.log(userInfo);
 
     const handleLogout = () => {
         const isLogout = confirm("정말 로그아웃 하시겠습니까?");
-        if (isLogout) return dispatch(logout());
+        if (isLogout) {
+            dispatch(logout());
+            navigate("/login");
+        }
     };
 
     useEffect(() => {
@@ -48,10 +52,12 @@ const NavBar = () => {
             <S.LeftDiv>
                 <S.NavButton onClick={() => navigate("/")}>Home</S.NavButton>{" "}
                 <S.NavButton $display={isLogin ? "block" : "none"} onClick={() => navigate("/mypage")}>내 프로필</S.NavButton>
-
             </S.LeftDiv>
             <S.RightDiv>
-                <S.Span $display={isLogin ? "block" : "none"}>{nickname}님 환영합니다!</S.Span>
+                <S.ProfileDiv $display={isLogin ? "flex" : "none"}>
+                    <S.ProfileImage src={avatar} />
+                    <S.Span>"{nickname}"님 환영합니다!</S.Span>
+                </S.ProfileDiv>
                 <S.Button $display={isLogin ? "none" : "block"} onClick={() => navigate("/login")}>로그인</S.Button>
                 <S.Button $display={isLogin ? "none" : "block"} onClick={() => navigate("/signup")} $color="green">회원가입</S.Button>
                 <S.Button $display={isLogin ? "block" : "none"} onClick={() => handleLogout()} $color="red">로그아웃</S.Button>
